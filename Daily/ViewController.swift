bb12b1c94ee4d9da6edbf94570088a81247b43cf
Victor Tejeda <victorfernandotejeda@gmail.com>
2023-05-09 23:51:00 -0400

    código agredo

--- Daily/ViewController.swift
+++ Daily/ViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var brView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        let colorTop = UIColor(red: 0.30, green: 0.63, blue: 0.69, alpha: 1.00).cgColor
+         let colorBottom = UIColor(red: 0.77, green: 0.88, blue: 0.90, alpha: 1.00).cgColor
+         
+         let gradientLayer = CAGradientLayer()
+         gradientLayer.frame = self.brView.bounds
+         gradientLayer.colors = [colorTop, colorBottom]
+         self.brView.layer.insertSublayer(gradientLayer, at: 0)    }
 
 
 }
