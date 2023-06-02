e25944e8561001421a89ffe6e6768053cf0f72cf
Victor Tejeda <victorfernandotejeda@gmail.com>
2023-06-02 00:17:22 -0400

    agregue varios layoutu en Ła primera view

--- Daily/Controller/ViewController.swift
+++ Daily/Controller/ViewController.swift
@@ -23,13 +23,45 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Logo.translatesAutoresizingMaskIntoConstraints = false
-               NSLayoutConstraint.activate([
-                   Logo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                   Logo.topAnchor.constraint(equalTo: view.topAnchor, constant: 160)
-               ])
+        // Configurar los constraints para los elementos
         
+        // Logo
+        Logo.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            Logo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            Logo.topAnchor.constraint(equalTo: view.topAnchor, constant: 160)
+        ])
+        
+        // nombreApp
+        nombreApp.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            nombreApp.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nombreApp.topAnchor.constraint(equalTo: Logo.bottomAnchor, constant: 20)
+        ])
+        
+        // iconoApp
+        iconoApp.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            iconoApp.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            iconoApp.topAnchor.constraint(equalTo: nombreApp.bottomAnchor, constant: 20)
+        ])
+        
+        // subtituloApp
+        subtituloApp.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            subtituloApp.leadingAnchor.constraint(equalTo: iconoApp.trailingAnchor, constant: 10),
+            subtituloApp.centerYAnchor.constraint(equalTo: iconoApp.centerYAnchor)
+        ])
+        
+        // icono2App
+        icono2App.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            icono2App.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            icono2App.topAnchor.constraint(equalTo: iconoApp.bottomAnchor, constant: -80)
+        ])
+        
+     
     }
-
 }
+
 
