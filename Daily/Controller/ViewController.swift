b82d4a1371c87eac0a6d32f7934253bd116a706c
Victor Tejeda <victorfernandotejeda@gmail.com>
2023-07-06 02:22:54 -0400

    eh arreglados algunas constraint y autolayout de las dos primeras vistas. pero ahi vamos

--- Daily/Controller/ViewController.swift
+++ Daily/Controller/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
     @IBOutlet weak var Logo: UIImageView!
     
     @IBOutlet weak var nombreApp: UIImageView!
@@ -20,48 +22,74 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonNegro: UIView!
     @IBOutlet weak var condionesYterminos: UIView!
     
+    @IBOutlet weak var botonazul: UIButton!
+    
+    @IBOutlet weak var botonnegro: UIButton!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Configurar los constraints para los elementos
+        botonazul.layer.cornerRadius = 22
+        // para quitar los bordes feos que sobresalen
+        botonazul.layer.masksToBounds = true
+    
+        botonnegro.layer.cornerRadius = 22
+        botonnegro.layer.masksToBounds = true
         
-        // Logo
-        Logo.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            Logo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            Logo.topAnchor.constraint(equalTo: view.topAnchor, constant: 160)
-        ])
         
-        // nombreApp
-        nombreApp.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            nombreApp.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            nombreApp.topAnchor.constraint(equalTo: Logo.bottomAnchor, constant: 20)
-        ])
         
-        // iconoApp
-        iconoApp.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            iconoApp.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            iconoApp.topAnchor.constraint(equalTo: nombreApp.bottomAnchor, constant: 20)
-        ])
         
-        // subtituloApp
-        subtituloApp.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            subtituloApp.leadingAnchor.constraint(equalTo: iconoApp.trailingAnchor, constant: 10),
-            subtituloApp.centerYAnchor.constraint(equalTo: iconoApp.centerYAnchor)
-        ])
-        
-        // icono2App
-        icono2App.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            icono2App.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            icono2App.topAnchor.constraint(equalTo: iconoApp.bottomAnchor, constant: -80)
-        ])
-        
-     
+////    // Logo
+//        Logo.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            Logo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            Logo.topAnchor.constraint(equalTo: view.topAnchor, constant: 160)
+//        ])
+//
+//        // nombreApp
+//        nombreApp.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            nombreApp.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            nombreApp.topAnchor.constraint(equalTo: Logo.bottomAnchor, constant: 50)
+//        ])
+//
+//        subtituloApp.translatesAutoresizingMaskIntoConstraints =  false
+//        NSLayoutConstraint.activate([
+//            subtituloApp.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            subtituloApp.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+//        ])
+//
+//        iconoApp.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            iconoApp.trailingAnchor.constraint(equalTo: subtituloApp.leadingAnchor),
+//            iconoApp.centerYAnchor.constraint(equalTo: subtituloApp.centerYAnchor)
+//        ])
+//        //        Use restriciones con el lenguaje de formato visual https://developer.apple.com/library/archive/documentation/UserExperience/Conceptual/AutolayoutPG/ProgrammaticallyCreatingConstraints.html#//apple_ref/doc/uid/TP40010853-CH16-SW1
+//        //        ese es el enlace.
+//
+//        icono2App.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:[subtituloApp]-[icono2App]", options: [], metrics: nil, views: ["subtituloApp": subtituloApp!, "icono2App": icono2App! ]))
+//        NSLayoutConstraint.activate([
+//            icono2App.centerYAnchor.constraint(equalTo: subtituloApp.centerYAnchor, constant: 20)
+//        ])
+//
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+////            button.topAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+////            button.topAnchor.constraint(equalTo: icono2App.bottomAnchor, constant: 50),
+//            button.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 80),
+//            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 22),
+////            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 17)
+////            button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+//        ])
+//        buttonNegro.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            nombreApp.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            nombreApp.topAnchor.constraint(equalTo: button.bottomAnchor)
+//        ])
     }
+    
 }
-
 
